4922b8da454c51ead83ad6faed129e4a182aac1d
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 13:50:35 +0900

    gugudan

--- Day03.py
+++ Day03.py
@@ -1,15 +1,14 @@
-subjucts = "    python, data structure, database    "
-print(len(subjucts))
-print(subjucts.strip())
-print(subjucts.find("data"), subjucts.index("data"))
-print(subjucts.find("dadfea")) #-1으로 return
-#print(subjucts.index("dsfadasfda")) #error로 return
-print(subjucts.rfind("data")) #뒤에서부터 찾는다
-print(subjucts.count("data")) #단어가 몇개인지 센다
-print(subjucts.center(40)) #문자 정렬 center,ljust,rjust가 있다
-blurt = "what the fuck...?!"
-print(blurt.strip("!?."))
-print(blurt.capitalize()) #앞글자만 대문자
-print(blurt.title()) #첫글자는 다 대문자
-print(blurt.upper()) #모든글자 대문자
-print(blurt.lower()) #모든글자 소문자
+
+
+while True:
+    dan = int(input("Dan(0) to quit : "))
+    if dan == 0:
+        break
+    elif 1 < dan < 10:
+        i = 1
+        while i < 10:
+            print("{0}*{1} = {2}\n".format(dan , i ,dan*i))
+            i += 1
+    else:
+        print("2에서 9사이의 숫자를 입력하세요")
+
