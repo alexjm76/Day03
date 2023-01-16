0bd69f4ad23099f7208a665f73e09c6288d71c7c
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 11:32:37 +0900

    string grammar

--- Day03.py
+++ Day03.py
@@ -1,6 +1,15 @@
-pokemons_list = ["피카츄","꼬북이","이상해","파이리"]
-pokemons_string = " , ".join(pokemons_list)
-print(pokemons_string)
-print(type(pokemons_string)) #str
-print(type(pokemons_list)) #list
-
+subjucts = "    python, data structure, database    "
+print(len(subjucts))
+print(subjucts.strip())
+print(subjucts.find("data"), subjucts.index("data"))
+print(subjucts.find("dadfea")) #-1으로 return
+#print(subjucts.index("dsfadasfda")) #error로 return
+print(subjucts.rfind("data")) #뒤에서부터 찾는다
+print(subjucts.count("data")) #단어가 몇개인지 센다
+print(subjucts.center(40)) #문자 정렬 center,ljust,rjust가 있다
+blurt = "what the fuck...?!"
+print(blurt.strip("!?."))
+print(blurt.capitalize()) #앞글자만 대문자
+print(blurt.title()) #첫글자는 다 대문자
+print(blurt.upper()) #모든글자 대문자
+print(blurt.lower()) #모든글자 소문자
