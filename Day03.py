ab5cfc1cdf55a25cc63389553c0b9e40f112d80a
Jungmoo Yoon <alexjm@naver.com>
2023-01-16 10:18:02 +0900

    1

---
+++ Day03.py
@@ -0,0 +1,11 @@
+#army = "우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.\n하나 우리는\t 자유 민주주의"
+#print(army)
+
+a=98.6
+b=str(a)
+print(type(b))
+
+start = "Na"*4+"\n"
+middle = "Hey"*3+"\n"
+end = "Goodbye"
+print(start+start+middle+end)
